266584611d542372fc8440df835abb01be6c6789
Daxalk <113640801+Daxalk@users.noreply.github.com>
2022-10-27 12:47:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def caesar(key, string, action):
         return 0
     encryption = []
     for symbol in string:
-        encryption.append((ord(symbol) + (key*sign)) % 65536)
+        encryption.append((ord(symbol) + (key * sign)) % 65536)
     message = ""
     for number in encryption:
         message += chr(number)
@@ -24,7 +24,7 @@ def frequency(text):
             space_index = key
     space_decrypt = ord(' ')
     space_encrypt = ord(space_index)
-    key = abs(space_decrypt-space_encrypt)
+    key = abs(space_decrypt - space_encrypt)
     message = caesar(key, text, 'decrypt')
     return message
 
@@ -35,27 +35,40 @@ def vigenere(key, string):
     for symbol in string:
         tmp1 = bin(ord(symbol) % 65536)[2::]
         if len(tmp1) < 8:
-            encryption.append('0'*(8-len(tmp1)) + tmp1)
+            encryption.append('0' * (8 - len(tmp1)) + tmp1)
         elif len(tmp1) == 8:
             encryption.append(tmp1)
         else:
             return 0
-
     for symbol in key:
         tmp2 = bin(ord(symbol) % 65536)[2::]
         if len(tmp2) < 8:
-            key_enc.append('0'*(8-len(tmp2)) + tmp2)
+            key_enc.append('0' * (8 - len(tmp2)) + tmp2)
         elif len(tmp2) == 8:
             key_enc.append(tmp2)
         else:
             return 0
+    encryption = xor(encryption, key_enc)
+    message = ""
+    for number in encryption:
+        message += chr(number)
+        print(number, chr(number))
+    return message
 
-    for i in range(len(encryption)):
-        encryption[i] = (encryption[i] and not key_enc[i]) or (not encryption[i] and key_enc[i])
 
-
-
-    print(encryption)
+def xor(x1, x2):
+    x = ''
+    x1, x2 = "".join(x1), "".join(x2)
+    result = []
+    for i in range(len(x1)):
+        if ((x1[i] + x2[i]) == '01') or ((x1[i] + x2[i]) == '10'):
+            x += '1'
+        else:
+            x += '0'
+        if len(x) == 8:
+            result.append(int(x, 2) % 65536)
+            x = ''
+    return result
 
 
 def main():
@@ -88,16 +101,29 @@ def main():
                 text = frequency(text)
                 print(f"Полученный текст: {text}")
             elif command_main == 3:
-                print("Введите строку")
-                string = input()
-                vigenere('SYSTEM', string)
+                print("1: Закодировать сообщение\n2: Декодировать сообщение\n3: Вернуться к выбору шифрования")
+                command_task = int(input())
+                if command_task == 1 or command_task == 2:
+                    print("Введите строку:")
+                    string = input()
+                    print("Введите ключ такой же длины как и строка")
+                    key = input()
+                    while len(key) != len(string):
+                        print("Разная длина у строки и ключа")
+                        print("Введите ключ такой же длины как и строка")
+                        key = input()
+                    message = vigenere(key, string)
+                    print(f"Полученная строка: {message}")
+                elif command_task == 3:
+                    pass
+                else:
+                    print("Такой команды нет!")
             elif command_main == 0:
                 print("Конец программы!")
                 break
         except ValueError:
             print("Некорректный ввод данных")
+            pass
 
 
 main()
-# fsdl;f;kdsl lflsdlk lfslkdflsdflk vsvs fsf     fdsf fsd   fsd f  fdsfsfdsf    f   s   s
-# kxiq@k@pixq%qkqxiqp%qkxqpikqxikqp%{x{x%kxk%%%%%kixk%kxi%%%kxi%k%%kixkxkixk%%%%k%%%x%%%x
